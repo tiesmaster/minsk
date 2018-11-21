d6cc34712dc696b9fab90e05c6921833b00ec270
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-11-21 14:52:22 +0100

    Add IlBackedEvaluator, and deps.

---
+++ src/Minsk/CodeAnalysis/HostMethodBuilder.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace Minsk.CodeAnalysis
+{
+    internal class VariableDef
+    {
+        public VariableDef(VariableSymbol variable, int slot)
+        {
+            Variable = variable;
+            Slot = slot;
+        }
+
+        public VariableSymbol Variable { get; }
+        public int Slot { get; }
+        public int VariableIndex => Slot - 1;
+    }
+
+    internal class HostMethodBuilder
+    {
+        private const string _hostAssemblyName = "HostAssembly";
+        private const string _hostTypeName = "HostType";
+        private const string _hostMethodName = "HostMethod";
+
+        private readonly AssemblyDefinition _hostAssemblyDefinition;
+
+        private readonly MethodDefinition _hostMethodDefinition;
+        private readonly Instruction _dummyJumpInstruction;
+
+        private readonly Dictionary<VariableSymbol, int> _variables = new Dictionary<VariableSymbol, int>();
+        private readonly List<(Instruction, LabelSymbol)> _jumpPatchList = new List<(Instruction, LabelSymbol)>();
+        private readonly Dictionary<LabelSymbol, Instruction> _labelMapping = new Dictionary<LabelSymbol, Instruction>();
+
+        // first variable slot is the result variable
+        private int _nextFreeVariableSlot = 1;
+
+        public HostMethodBuilder()
+        {
+            _hostAssemblyDefinition = AssemblyDefinition.CreateAssembly(
+                new AssemblyNameDefinition(_hostAssemblyName, new Version(1, 0, 0, 0)), _hostAssemblyName, ModuleKind.Dll);
+
+            HostModule = _hostAssemblyDefinition.MainModule;
+
+            var hostTypeDefinition = new TypeDefinition(null, _hostTypeName,
+                TypeAttributes.Class | TypeAttributes.Public, TypeSystem.Object);
+
+            HostModule.Types.Add(hostTypeDefinition);
+
+            _hostMethodDefinition = new MethodDefinition(_hostMethodName,
+                MethodAttributes.Public | MethodAttributes.Static, TypeSystem.Object);
+
+            _hostMethodDefinition.Parameters.Add(
+                new ParameterDefinition("variables", ParameterAttributes.None, HostModule.ImportReference(typeof(object[]))));
+
+            hostTypeDefinition.Methods.Add(_hostMethodDefinition);
+
+            HostMethodIlProcessor = _hostMethodDefinition.Body.GetILProcessor();
+
+            _dummyJumpInstruction = HostMethodIlProcessor.Create(OpCodes.Nop);
+            AddResultVariable();
+        }
+
+        public ILProcessor HostMethodIlProcessor { get; }
+        public ModuleDefinition HostModule { get; }
+        public TypeSystem TypeSystem => HostModule.TypeSystem;
+
+        public IEnumerable<VariableDef> Variables => _variables.Select(kvp => new VariableDef(kvp.Key, kvp.Value));
+
+        public HostMethod Build()
+        {
+            PatchupJumps();
+            return CreateHostMethod();
+        }
+
+        private void PatchupJumps()
+        {
+            foreach (var (jump, label) in _jumpPatchList)
+            {
+                var targetInstruction = _labelMapping[label].Next;
+                jump.Operand = targetInstruction;
+            }
+        }
+
+        private HostMethod CreateHostMethod()
+        {
+            using (var ms = new MemoryStream())
+            {
+                _hostAssemblyDefinition.Write(ms);
+
+                var peBytes = ms.ToArray();
+                var assembly = System.Reflection.Assembly.Load(peBytes);
+
+                return new HostMethod(assembly, _hostTypeName, _hostMethodName);
+            }
+        }
+
+        public int GetOrCreateVariableSlot(VariableSymbol variable)
+        {
+            if (_variables.TryGetValue(variable, out var existingSlot))
+            {
+                return existingSlot;
+            }
+            var freeSlot = _nextFreeVariableSlot++;
+
+            _variables[variable] = freeSlot;
+            AddVariable(variable.Type);
+
+            return freeSlot;
+        }
+
+        private void AddResultVariable()
+        {
+            _hostMethodDefinition.Body.Variables.Add(new VariableDefinition(TypeSystem.Object));
+        }
+
+        private void AddVariable(Type variableType)
+        {
+            _hostMethodDefinition.Body.Variables.Add(new VariableDefinition(HostModule.ImportReference(variableType)));
+        }
+
+        public void MarkLabel(LabelSymbol label)
+        {
+            _labelMapping[label] = HostMethodIlProcessor.Body.Instructions.LastOrDefault();
+        }
+
+        public void AddJump(OpCode jumpOpcode, LabelSymbol jumpLabel)
+        {
+            var jumpInstruction = HostMethodIlProcessor.Create(jumpOpcode, _dummyJumpInstruction);
+            _jumpPatchList.Add((jumpInstruction, jumpLabel));
+
+            HostMethodIlProcessor.Append(jumpInstruction);
+        }
+    }
+}
---
+++ src/Minsk/CodeAnalysis/IlBackedEvaluator.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Minsk.CodeAnalysis.Binding;
+using Mono.Cecil.Cil;
+
+namespace Minsk.CodeAnalysis
+{
+    internal sealed partial class IlBackedEvaluator
+    {
+        private readonly BoundBlockStatement _root;
+        private readonly Dictionary<VariableSymbol, object> _variables;
+
+        private HostMethodBuilder _ilBuilder;
+        private ILProcessor _il;
+
+        public IlBackedEvaluator(BoundBlockStatement root, Dictionary<VariableSymbol, object> variables)
+        {
+            _root = root;
+            _variables = variables;
+        }
+
+        public object Evaluate()
+        {
+            Initialize();
+            EmitHostMethod();
+            var result = InvokeHostMethod(_ilBuilder.Build());
+
+            return result;
+        }
+
+        private void Initialize()
+        {
+            _ilBuilder = new HostMethodBuilder();
+            _il = _ilBuilder.HostMethodIlProcessor;
+        }
+
+        private void EmitHostMethod()
+        {
+            EmitBlockStatement(_root);
+
+            InsertEmitRestoreVariablesFromArgumentToStartOfMethod();
+            EmitSaveVariables();
+
+            EmitPushResult();
+            EmitEndOfMethod();
+        }
+
+        private void EmitPushResult()
+        {
+            _il.Emit(OpCodes.Ldloc_0);
+        }
+
+        private void InsertEmitRestoreVariablesFromArgumentToStartOfMethod()
+        {
+            void InsertInstructionsAtBeginningOfMethod(ILProcessor il, IEnumerable<Instruction> instructions)
+            {
+                foreach (var instruction in instructions.Reverse())
+                {
+                    var firstInstruction = il.Body.Instructions.First();
+                    il.InsertBefore(firstInstruction, instruction);
+                }
+            }
+
+            var instructionsToInsert = new List<Instruction>();
+            foreach (var variableDef in _ilBuilder.Variables)
+            {
+                // load variables[i] from arguments, and unbox
+                instructionsToInsert.Add(_il.Create(OpCodes.Ldarg_0));
+                instructionsToInsert.Add(_il.Create(OpCodes.Ldc_I4, variableDef.VariableIndex));
+                instructionsToInsert.Add(_il.Create(OpCodes.Ldelem_Ref));
+                instructionsToInsert.Add(_il.Create(OpCodes.Unbox_Any, _ilBuilder.HostModule.ImportReference(variableDef.Variable.Type)));
+
+                // and store in given slot
+                instructionsToInsert.Add(_il.Create(OpCodes.Stloc, variableDef.Slot));
+
+                InsertInstructionsAtBeginningOfMethod(_il, instructionsToInsert);
+                instructionsToInsert.Clear();
+            }
+        }
+
+        private void EmitSaveVariables()
+        {
+            foreach (var variableDef in _ilBuilder.Variables)
+            {
+                // load the in|out variable array
+                _il.Emit(OpCodes.Ldarg_0);
+                // index into the variable array
+                _il.Emit(OpCodes.Ldc_I4, variableDef.VariableIndex);
+
+                // load the variable from the given slot, and box it
+                _il.Emit(OpCodes.Ldloc, variableDef.Slot);
+                _il.Emit(OpCodes.Box, _ilBuilder.HostModule.ImportReference(variableDef.Variable.Type));
+
+                _il.Emit(OpCodes.Stelem_Ref);
+            }
+        }
+
+        private void EmitEndOfMethod()
+        {
+            _il.Emit(OpCodes.Ret);
+        }
+    }
+}
