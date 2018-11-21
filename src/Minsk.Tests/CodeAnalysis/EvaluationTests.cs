e4a6695ae3edb3f35f45839e94a721e7c84ecbf7
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-11-21 15:01:17 +0100

    Feature toggle in the jit backend.

--- src/Minsk.Tests/CodeAnalysis/EvaluationTests.cs
+++ src/Minsk.Tests/CodeAnalysis/EvaluationTests.cs
@@ -9,74 +9,76 @@ namespace Minsk.Tests.CodeAnalysis
     public class EvaluationTests
     {
         [Theory]
-        [InlineData("1", 1)]
-        [InlineData("+1", 1)]
-        [InlineData("-1", -1)]
-        [InlineData("~1", -2)]
-        [InlineData("14 + 12", 26)]
-        [InlineData("12 - 3", 9)]
-        [InlineData("4 * 2", 8)]
-        [InlineData("9 / 3", 3)]
-        [InlineData("(10)", 10)]
-        [InlineData("12 == 3", false)]
-        [InlineData("3 == 3", true)]
-        [InlineData("12 != 3", true)]
-        [InlineData("3 != 3", false)]
-        [InlineData("3 < 4", true)]
-        [InlineData("5 < 4", false)]
-        [InlineData("4 <= 4", true)]
-        [InlineData("4 <= 5", true)]
-        [InlineData("5 <= 4", false)]
-        [InlineData("4 > 3", true)]
-        [InlineData("4 > 5", false)]
-        [InlineData("4 >= 4", true)]
-        [InlineData("5 >= 4", true)]
-        [InlineData("4 >= 5", false)]
-        [InlineData("1 | 2", 3)]
-        [InlineData("1 | 0", 1)]
-        [InlineData("1 & 3", 1)]
-        [InlineData("1 & 0", 0)]
-        [InlineData("1 ^ 0", 1)]
-        [InlineData("0 ^ 1", 1)]
-        [InlineData("1 ^ 3", 2)]
-        [InlineData("false == false", true)]
-        [InlineData("true == false", false)]
-        [InlineData("false != false", false)]
-        [InlineData("true != false", true)]
-        [InlineData("true && true", true)]
-        [InlineData("false || false", false)]
-        [InlineData("false | false", false)]
-        [InlineData("false | true", true)]
-        [InlineData("true | false", true)]
-        [InlineData("true | true", true)]
-        [InlineData("false & false", false)]
-        [InlineData("false & true", false)]
-        [InlineData("true & false", false)]
-        [InlineData("true & true", true)]
-        [InlineData("false ^ false", false)]
-        [InlineData("true ^ false", true)]
-        [InlineData("false ^ true", true)]
-        [InlineData("true ^ true", false)]
-        [InlineData("true", true)]
-        [InlineData("false", false)]
-        [InlineData("!true", false)]
-        [InlineData("!false", true)]
-        [InlineData("var a = 10", 10)]
-        [InlineData("{ var a = 10 (a * a) }", 100)]
-        [InlineData("{ var a = 0 (a = 10) * a }", 100)]
-        [InlineData("{ var a = 0 if a == 0 a = 10 a }", 10)]
-        [InlineData("{ var a = 0 if a == 4 a = 10 a }", 0)]
-        [InlineData("{ var a = 0 if a == 0 a = 10 else a = 5 a }", 10)]
-        [InlineData("{ var a = 0 if a == 4 a = 10 else a = 5 a }", 5)]
-        [InlineData("{ var i = 10 var result = 0 while i > 0 { result = result + i i = i - 1} result }", 55)]
-        [InlineData("{ var result = 0 for i = 1 to 10 { result = result + i } result }", 55)]
-        public void Evaluator_Computes_CorrectValues(string text, object expectedValue)
+        [FeatureToggledInlineData("1", 1)]
+        [FeatureToggledInlineData("+1", 1)]
+        [FeatureToggledInlineData("-1", -1)]
+        [FeatureToggledInlineData("~1", -2)]
+        [FeatureToggledInlineData("14 + 12", 26)]
+        [FeatureToggledInlineData("12 - 3", 9)]
+        [FeatureToggledInlineData("4 * 2", 8)]
+        [FeatureToggledInlineData("9 / 3", 3)]
+        [FeatureToggledInlineData("(10)", 10)]
+        [FeatureToggledInlineData("12 == 3", false)]
+        [FeatureToggledInlineData("3 == 3", true)]
+        [FeatureToggledInlineData("12 != 3", true)]
+        [FeatureToggledInlineData("3 != 3", false)]
+        [FeatureToggledInlineData("3 < 4", true)]
+        [FeatureToggledInlineData("5 < 4", false)]
+        [FeatureToggledInlineData("4 <= 4", true)]
+        [FeatureToggledInlineData("4 <= 5", true)]
+        [FeatureToggledInlineData("5 <= 4", false)]
+        [FeatureToggledInlineData("4 > 3", true)]
+        [FeatureToggledInlineData("4 > 5", false)]
+        [FeatureToggledInlineData("4 >= 4", true)]
+        [FeatureToggledInlineData("5 >= 4", true)]
+        [FeatureToggledInlineData("4 >= 5", false)]
+        [FeatureToggledInlineData("1 | 2", 3)]
+        [FeatureToggledInlineData("1 | 0", 1)]
+        [FeatureToggledInlineData("1 & 3", 1)]
+        [FeatureToggledInlineData("1 & 0", 0)]
+        [FeatureToggledInlineData("1 ^ 0", 1)]
+        [FeatureToggledInlineData("0 ^ 1", 1)]
+        [FeatureToggledInlineData("1 ^ 3", 2)]
+        [FeatureToggledInlineData("false == false", true)]
+        [FeatureToggledInlineData("true == false", false)]
+        [FeatureToggledInlineData("false != false", false)]
+        [FeatureToggledInlineData("true != false", true)]
+        [FeatureToggledInlineData("true && true", true)]
+        [FeatureToggledInlineData("false || false", false)]
+        [FeatureToggledInlineData("false | false", false)]
+        [FeatureToggledInlineData("false | true", true)]
+        [FeatureToggledInlineData("true | false", true)]
+        [FeatureToggledInlineData("true | true", true)]
+        [FeatureToggledInlineData("false & false", false)]
+        [FeatureToggledInlineData("false & true", false)]
+        [FeatureToggledInlineData("true & false", false)]
+        [FeatureToggledInlineData("true & true", true)]
+        [FeatureToggledInlineData("false ^ false", false)]
+        [FeatureToggledInlineData("true ^ false", true)]
+        [FeatureToggledInlineData("false ^ true", true)]
+        [FeatureToggledInlineData("true ^ true", false)]
+        [FeatureToggledInlineData("true", true)]
+        [FeatureToggledInlineData("false", false)]
+        [FeatureToggledInlineData("!true", false)]
+        [FeatureToggledInlineData("!false", true)]
+        [FeatureToggledInlineData("var a = 10", 10)]
+        [FeatureToggledInlineData("{ var a = 10 (a * a) }", 100)]
+        [FeatureToggledInlineData("{ var a = 0 (a = 10) * a }", 100)]
+        [FeatureToggledInlineData("{ var a = 0 if a == 0 a = 10 a }", 10)]
+        [FeatureToggledInlineData("{ var a = 0 if a == 4 a = 10 a }", 0)]
+        [FeatureToggledInlineData("{ var a = 0 if a == 0 a = 10 else a = 5 a }", 10)]
+        [FeatureToggledInlineData("{ var a = 0 if a == 4 a = 10 else a = 5 a }", 5)]
+        [FeatureToggledInlineData("{ var i = 10 var result = 0 while i > 0 { result = result + i i = i - 1} result }", 55)]
+        [FeatureToggledInlineData("{ var result = 0 for i = 1 to 10 { result = result + i } result }", 55)]
+        public void Evaluator_Computes_CorrectValues(string text, object expectedValue, bool useJitting)
         {
-            AssertValue(text, expectedValue);
+            AssertValue(text, expectedValue, useJitting);
         }
 
-        [Fact]
-        public void Evaluator_ContinueWith_CarriesOverVariablesBetweenCompilations()
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Evaluator_ContinueWith_CarriesOverVariablesBetweenCompilations(bool useJitting)
         {
             // arrange
             var firstSubmission = SyntaxTree.Parse("var a = 2");
@@ -85,10 +87,10 @@ namespace Minsk.Tests.CodeAnalysis
 
             // act
             var compilation = new Compilation(firstSubmission);
-            var firstResult = compilation.Evaluate(variables);
+            var firstResult = compilation.Evaluate(variables, useJitting);
 
             compilation = compilation.ContinueWith(secondSubmission);
-            var secondResult = compilation.Evaluate(variables);
+            var secondResult = compilation.Evaluate(variables, useJitting);
 
             // assert
             Assert.Equal(2, firstResult.Value);
@@ -298,12 +300,12 @@ namespace Minsk.Tests.CodeAnalysis
             AssertDiagnostics(text, diagnostics);
         }
 
-        private static void AssertValue(string text, object expectedValue)
+        private static void AssertValue(string text, object expectedValue, bool useJitting = false)
         {
             var syntaxTree = SyntaxTree.Parse(text);
             var compilation = new Compilation(syntaxTree);
             var variables = new Dictionary<VariableSymbol, object>();
-            var result = compilation.Evaluate(variables);
+            var result = compilation.Evaluate(variables, useJitting);
 
             Assert.Empty(result.Diagnostics);
             Assert.Equal(expectedValue, result.Value);
