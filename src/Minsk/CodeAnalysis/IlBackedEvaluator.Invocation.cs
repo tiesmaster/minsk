6938cb3bfa85a430a52e44ceb5158a32c8ec5fbb
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-12-04 00:31:22 +0100

    Extract emitting, and invocation of hostmethod into own files.

---
+++ src/Minsk/CodeAnalysis/IlBackedEvaluator.Invocation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Minsk.CodeAnalysis
+{
+    internal sealed partial class IlBackedEvaluator
+    {
+        private object InvokeHostMethod(HostMethod hostMethod)
+        {
+            var variableValues = CreateVariablesParameter();
+            var result = hostMethod.Invoke(variableValues);
+            CopyVariablesBackToDictionary(variableValues);
+
+            return result;
+        }
+
+        private object[] CreateVariablesParameter()
+        {
+            return (from variableDef in _ilBuilder.Variables
+                    orderby variableDef.Slot
+                    let variable = variableDef.Variable
+                    select _variables.TryGetValue(variable, out var value)
+                        ? value
+                        : Activator.CreateInstance(variable.Type)
+            ).ToArray();
+        }
+
+        private void CopyVariablesBackToDictionary(object[] variableValues)
+        {
+            foreach (var variableDef in _ilBuilder.Variables)
+            {
+                _variables[variableDef.Variable] = variableValues[variableDef.VariableIndex];
+            }
+        }
+    }
+}
